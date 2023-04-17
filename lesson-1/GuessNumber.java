f76e6ba6050fa65cba5b200a4f62ddc5ccfbb294
rusokolov4 <sokolovry@yandex.ru>
2023-04-18 00:01:13 +0300

    rename file Guessing.java to GuessNumber.java

--- lesson-1/GuessNumber.java
+++ lesson-1/GuessNumber.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Guessing {
+public class GuessNumber {
     public static void main(String[] args) {
         Random random = new Random();
         int randomNumber = random.nextInt(100) + 1;
