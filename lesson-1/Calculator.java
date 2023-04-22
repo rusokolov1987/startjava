f56121638144aa0e78d9fabf2c8589ae021566d6
rusokolov4 <sokolovry@yandex.ru>
2023-04-22 10:13:56 +0300

    corrected Calculator.java

--- lesson-1/Calculator.java
+++ lesson-1/Calculator.java
@@ -2,49 +2,53 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = (int) (Math.random() * 10);
         int num2 = (int) (Math.random() * 10);
+        int result = 0;
         char sign = '^';
         switch (sign) {
             case '+':
-                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                result = num1 + num2;
                 break;
             case '-':
-                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                result = num1 - num2;
                 break;
             case '/':
                 if (num2 == 0) {
-                    System.out.println("Деление на ноль!");
                     break;
                 }
-                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                result = num1 / num2;
                 break;
             case '*':
-                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                result = num1 * num2;
                 break;
             case '%':
                 if (num2 == 0) {
-                    System.out.println("Деление на ноль!");
                     break;
                 }
-                System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+                result = num1 % num2;
                 break;
             case '^':
                 if (num1 == 1 || num1 == 0) {
-                    System.out.println(num1 + " ^ " + num2 + " = " + num1);
+                    result = num1;
                     break;
                 }
                 if (num2 == 0) {
-                    System.out.println(num1 + " ^ " + num2 + " = 1");
+                    result = 1;
                     break;
                 }
                 int multiply = 1;
                 for (int i = 0; i < num2; i++) {
                     multiply *= num1;
                 }
-                System.out.println(num1 + " ^ " + num2 + " = " + multiply);
+                result = multiply;
                 break;
             default:
                 System.out.println("Такой операции нет!");
                 break;
+        }
+        if (sign == '%' | sign == '/' && num2 == 0) {
+            System.out.println("Деление на ноль!");
+        } else {
+            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         }
     }
 }
