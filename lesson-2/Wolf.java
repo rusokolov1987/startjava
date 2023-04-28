0faa14d7e91beaeaeec30c0adc535b438604439f
rusokolov4 <sokolovry@yandex.ru>
2023-04-28 23:55:20 +0300

    corrected Wolf.java and WolfTest.java according to exercise #5

--- lesson-2/Wolf.java
+++ lesson-2/Wolf.java
@@ -1,10 +1,54 @@
 public class Wolf {
 
-    String sex;
-    String name;
-    double weight;
-    int age;
-    String color;
+    private String sex;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
 
     public void move() {
         System.out.println("Идёт");
--- lesson-2/WolfTest.java
+++ lesson-2/WolfTest.java
@@ -1,13 +1,13 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.sex = "самец";
-        wolf.name = "Роан";
-        wolf.weight = 24.3;
-        wolf.age = 2;
-        wolf.color = "серый";
-        System.out.println("Пол волка: " + wolf.sex + "\nКличка: " + wolf.name + "\nВозраст: " + 
-            wolf.age + "\nВес: " + wolf.weight + "\nЦвет: " + wolf.color);
+        wolf.setSex("самец");
+        wolf.setName("Роан");
+        wolf.setWeight(24.3);
+        wolf.setAge(2);
+        wolf.setColor("серый");
+        System.out.println("Пол волка: " + wolf.getSex() + "\nКличка: " + wolf.getName() +
+            "\nВозраст: " + wolf.getAge() + "\nВес: " + wolf.getWeight() + "\nЦвет: " + wolf.getColor());
         wolf.move();
         wolf.sit();
         wolf.run();
