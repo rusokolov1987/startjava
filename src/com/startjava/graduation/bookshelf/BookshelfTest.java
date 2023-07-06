12a8a9dbafbb010ebaac424c2fd1d1af0ed00822
sokolovry <sokolovry@yandex.ru>
2023-07-06 23:54:08 +0300

    fix: coorect BookshelfTest and Bookshelf

--- src/com/startjava/graduation/bookshelf/BookshelfTest.java
+++ src/com/startjava/graduation/bookshelf/BookshelfTest.java
@@ -4,86 +4,91 @@ import java.util.Scanner;
 
 public class BookshelfTest {
     public static void main(String[] args) {
-        String info = """
+        Bookshelf bookshelf = new Bookshelf();
+        int command;
+        while (true) {
+            showBookCase(bookshelf);
+            menu();
+            try {
+                command = Integer.parseInt(selectMenuItem());
+                switch (command) {
+                    case 1:
+                        addBook(bookshelf);
+                        break;
+                    case 2:
+                        deleteBook(bookshelf);
+                        break;
+                    case 3:
+                        showBook(bookshelf);
+                        break;
+                    case 4:
+                        bookshelf.clean();
+                        System.out.println("Шкаф пуст!");
+                        break;
+                    case 5:
+                        System.out.println("Вы закрыли шкаф! Всего доброго!");
+                        return;
+                    default:
+                        System.out.println("Такого пункта в меню нет!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный формат ввода!");
+            }
+            inputEnter();
+        }
+    }
+
+    private static void menu() {
+        System.out.println("""
+                    Меню
                 1. Добавить
                 2. Удалить
                 3. Найти
-                4. Показать все книги
-                5. Количество книг в шкафу
-                6. Количество пустых полок
-                7. Очитсить шкаф
-                8. Выйти
-                """;
-        menu(info);
+                4. Очистить шкаф
+                5. Выйти
+                """);
     }
 
-    private static void menu(String info) {
-        Bookshelf bookshelf = new Bookshelf();
-        while (true) {
-            if (bookshelf.getCountBooks() == 0) {
-                System.out.println("Шкаф пуст! Вы можете добавить первую книгу!");
-            } else {
-                System.out.println("В шкафу " + bookshelf.getCountBooks() + " книг и сивободно " +
-                        bookshelf.getEmptyShelfs() + " полок.");
-                bookshelf.showBookshelf();
-            }
-            int command;
-            while (true) {
-                System.out.printf("%10s", "Меню\n");
-                System.out.println(info);
-                try {
-                    command = Integer.parseInt(input("Введите номер операции!"));
-                    break;
-                } catch (NumberFormatException ex) {
-                    System.out.println("Такой операции не существует!");
-                }
-            }
-            switch (command) {
-                case 1:
-                    try {
-                        bookshelf.add(new Book(input("Введите автора"), input("Введите название книги"),
-                                Integer.parseInt(input("Введите дату публикации"))));
-                        System.out.println("Книга добавлена в шкаф!");
-                    } catch (RuntimeException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case 2:
-                    try {
-                        bookshelf.delete(input("Введите название книги, которую хотите убрать с полки."));
-                        System.out.println("Книга успешно удалена!");
-                    } catch (RuntimeException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                    break;
-                case 3:
-                    try {
-                        Book book = bookshelf.search(input("Введите название книги, которую хотите найти в шкафу."));
-                        System.out.println("Книга найдена: " + book);
-                    } catch (RuntimeException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                    break;
-                case 4:
-                    bookshelf.showBookshelf();
-                    break;
-                case 5:
-                    System.out.println("Количество книг в шкафу: " + bookshelf.getCountBooks());
-                    break;
-                case 6:
-                    System.out.println("Количество пустых полок в шкафу: " + bookshelf.getEmptyShelfs());
-                    break;
-                case 7:
-                    bookshelf.clean();
-                    System.out.println("Шкаф пуст!");
-                    break;
-                case 8:
-                    System.out.println("Вы вышли из программы!");
-                    System.exit(0);
-                default:
-                    System.out.println("Такой операции не существует!");
-            }
-            keyPressed();
+    private static String selectMenuItem() {
+        System.out.println("Введите номер пункта меню!");
+        return new Scanner(System.in).nextLine();
+    }
+
+    private static void showBookCase(Bookshelf bookshelf) {
+        if (bookshelf.getCountBooks() == 0) {
+            System.out.println("Шкаф пуст! Вы можете добавить первую книгу!");
+        } else {
+            System.out.println("В шкафу " + bookshelf.getCountBooks() + " книг и свободно " +
+                    bookshelf.getEmptyShelfs() + " полок.");
+            showBookshelf(bookshelf);
+        }
+    }
+
+    private static void addBook(Bookshelf bookshelf) {
+        try {
+            bookshelf.add(new Book(input("Введите автора"), input("Введите название книги"),
+                    Integer.parseInt(input("Введите дату публикации"))));
+            System.out.println("Книга добавлена в шкаф!");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void deleteBook(Bookshelf bookshelf) {
+        try {
+            bookshelf.delete(input("Введите название книги, которую хотите убрать с полки."));
+            System.out.println("Книга успешно удалена!");
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void showBook(Bookshelf bookshelf) {
+        try {
+            Book book = bookshelf.search(input("Введите название книги, которую хотите найти в шкафу."));
+            System.out.println("Книга найдена: " + book);
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
@@ -92,7 +97,7 @@ public class BookshelfTest {
         return new Scanner(System.in).nextLine();
     }
 
-    public static void showBookshelf(Bookshelf bookshelf) {
+    private static void showBookshelf(Bookshelf bookshelf) {
         int length = bookshelf.getBooks().length;
         Book[] books = bookshelf.getBooks();
         for (int i = 0; i < length; i++) {
@@ -105,7 +110,7 @@ public class BookshelfTest {
         }
     }
 
-    private static void keyPressed() {
+    private static void inputEnter() {
         while (true) {
             System.out.println("Нажмите Enter для продолжения работы!");
             String enter = new Scanner(System.in).nextLine();
